0acb5cc5c87b4527fba932d854d96a8d18e4dfe8
Tatenda Munenge <tatemunenge@gmail.com>
2021-06-17 17:37:37 +0200

    display dynamic side bar content

---
+++ sidebar.php
@@ -0,0 +1,6 @@
+<<div id="sidebar-primary" class="sidebar">
+    <?php if ( is_active_sidebar( 'home_right_1' ) ) : ?>
+        <?php dynamic_sidebar( 'home_right_1' ); ?>
+        <!-- Time to add some widgets! -->
+    <?php endif; ?>
+</div>
