98823318ea961be2fe688208392adcd9c14b0230
Tatenda Munenge <tatemunenge@gmail.com>
2021-06-17 14:55:42 +0200

    fix single templates

--- page.php
+++ page.php
@@ -2,21 +2,19 @@
     get_header();
 ?>
 <main id="main">
-    <section >
-      <div class="container" data-aos="fade-up">
-        <div class="row">
-            <div class="col">
-	            <?php
-	            	if(have_posts()){
-	            		while(have_posts()){
-	            			the_post();
-	            			the_content();
-	            		}
+    <section class="inner-page">
+      <div class="container">
+        <p>
+            <?php
+	            if(have_posts()){
+	            	while(have_posts()){
+	            		the_post();
+	            		the_content();
 	            	}
-	            ?>
-            </div>
-        </div>
-     </div>
+	            }
+	        ?>
+        </p>
+      </div>
     </section>
 </main>
 
