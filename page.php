333da7b73476386c7d2909334aab2df5793402a7
Tatenda Munenge <tatemunenge@gmail.com>
2021-06-17 15:40:20 +0200

    add dynamic template parts

--- page.php
+++ page.php
@@ -8,8 +8,7 @@
             <?php
 	            if(have_posts()){
 	            	while(have_posts()){
-	            		the_post();
-	            		the_content();
+                        get_template_part( 'template-parts/side_barcontent');
 	            	}
 	            }
 	        ?>
---
+++ template-parts/side_barcontent.php
@@ -0,0 +1,4 @@
+<?php
+    the_post();
+    the_content();
+?>
